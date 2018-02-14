a209efff7ab0046662aeae5930a614b37fd28878
Tyge <tygesjohnson@gmail.com>
2018-02-14 12:04:39 -0700

    move components to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,110 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'; // new
+
 import './App.css';
-import Navigation from './components/Navigation';
-import { Home, About, Contact} from './components';
 
 const App = () => (
-  <div>
-    <Navigation /> 
-  </div>
-)
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="navlink--active" exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="navlink--active">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/products" activeClassName="navlink--active">
+            Products
+          </NavLink>
+        </li>
+      </ul>
+
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+      <Route path="/products" component={Products} />
+    </div>
+  </Router>
+);
+
+function About() {
+  return (
+    <div>
+      <h1>About Component</h1>
+      <p id="paragraph">This is the About Component</p>
+    </div>
+  );
+}
+
+function Movies() {
+  return (
+    <div>
+      <h1>Movies Component</h1>
+      <p id="paragraph">This is the Movies Component</p>
+    </div>
+  );
+}
+
+function Music() {
+  return (
+    <div>
+      <h1>Music Component</h1>
+      <p id="paragraph">This is the Music Component</p>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Component</h1>
+      <p id="paragraph">This is the Home Component</p>
+    </div>
+  );
+}
+
+function Products(props) {
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/products/movies" activeClassName="navlink--active">
+            Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/products/music" activeClassName="navlink--active">
+            Music
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/products/movies" component={Movies} />
+      <Route path="/products/music" component={Music} />
+      <hr />
+    </div>
+  );
+}
+
+
 
 export default App;
+
+// /friends/1
+
+// import React from 'react';
+// import './App.css';
+// import Navigation from './components/Navigation';
+// import { Home, About, Contact} from './components';
+//
+// const App = () => (
+//   <div>
+//     <Navigation />
+//   </div>
+// )
+//
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import App from "./App";
+
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+//
+// import './index.css';
+//
+// function About() {
+//   return (
+//     <div>
+//       <h1>About component</h1>
+//       <p id="paragraph">This is the about component</p>
+//     </div>
+//   );
+// }
+//
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home component</h1>
+//       <p id="paragraph">This is the home component</p>
+//     </div>
+//   );
+// }
+//
+// function Movies() {
+//   return (
+//     <div>
+//       <h1>movies component</h1>
+//       <p id="paragraph">This is the movies component</p>
+//     </div>
+//   );
+// }
+//
+// function Music() {
+//   return (
+//     <div>
+//       <h1>Music component</h1>
+//       <p id="paragraph">This is the music component</p>
+//     </div>
+//   );
+// }
+//
+// function Products(props) {
+//   return (
+//     <div>
+//       <ul>
+//         <li><NavLink to="/products/movies" activeClassName="navlink--active" exact>Movies</NavLink></li>
+//         <li><NavLink to="/products/music" activeClassName="navlink--active">Music</NavLink></li>
+//       </ul>
+//       <hr/>
+//       <Route path="/products/movies" component={Movies} />
+//       <Route path="/products/music" component={Music} />
+//     <hr/>
+//   </div>
+//   );
+// }
+//
+//
+//
+// ReactDOM.render(
+// <Router>
+//   <div>
+//     <ul>
+//       <li><NavLink to="/" activeClassName="navlink--active" exact>Home</NavLink></li>
+//       <li><NavLink to="/about" activeClassName="navlink--active">About</NavLink></li>
+//       <li><NavLink to="/products" activeClassName="navlink--active">products</NavLink></li>
+//
+//     </ul>
+//   <Route path="/" component={Home} exact/>
+//   <Route path="/about" component={About} />
+//   <Route path="/products" component={Products} />
+//
+//
+//
+// </div>
+// </Router>, document.getElementById('root'));
+//
+// // http://localhost:3000/
+//
+// // import app render app import css all in this file
